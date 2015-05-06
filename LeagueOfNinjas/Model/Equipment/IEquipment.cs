87a388a1f124748734ada24b93ec617875327d6b
Nevernown <ca.boel@student.avans.nl>
2015-05-06 14:53:14 +0200

    Something

---
+++ LeagueOfNinjas/Model/Equipment/IEquipment.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfNinjas.Model.Equipment
+{
+    public interface IEquipment
+    {
+        int Price { get; set; }
+        public int Strength { get; set; }
+        public int Agillity { get; set; }
+        public int Intelligence { get; set; }
+    }
+}
---
+++ LeagueOfNinjas/Model/Equipment/Shoulder.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeagueOfNinjas.Model.Equipment
+{
+    public class Shoulder : IEquipment
+    {
+        private int _price;
+        private int _str;
+        private int _dex;
+        private int _int;
+
+        public int Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public int Strength
+        {
+            get { return _str; }
+            set { _str = value; }
+        }
+
+        public int Agillity
+        {
+            get { return _dex; }
+            set { _dex = value; }
+        }
+
+        public int Intelligence
+        {
+            get { return _int; }
+            set { _int = value; }
+        }
+    }
+}
